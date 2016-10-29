5bfb62234b86d6ec0575ccf11b0f30c0b56935aa
Alexandre GUIDET <guidet.alexandre@gmail.com>
2016-10-29 17:25:05 +1300

    save

--- public/index.php
+++ public/index.php
@@ -2,8 +2,8 @@
 $roomWidth = 25;
 $roomHeight = 15;
 
-$width = $roomWidth * 5;
-$height = $roomHeight * 5;
+$width = $roomWidth * 4;
+$height = $roomHeight * 4;
 
 $tileSize = 16;
 
@@ -28,10 +28,50 @@ $tileSize = 16;
 <body>
 <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container-fluid">
-        <div class="btn-group" role="group">
-            <button type="button" class="btn btn-default navbar-btn">Write</button>
-            <button type="button" class="btn btn-default navbar-btn">Erase</button>
-            <button type="button" class="btn btn-default navbar-btn">Right</button>
+
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Online Map Designer</a>
+        </div>
+
+        <div class="collapse navbar-collapse">
+
+            <div class="btn-group" role="group">
+                <button type="button" class="btn btn-default navbar-btn">
+                    <span class="glyphicon glyphicon-file"></span>
+                </button>
+                <button type="button" class="btn btn-default navbar-btn">
+                    <span class="glyphicon glyphicon-folder-open"></span>
+                </button>
+                <button type="button" class="btn btn-default navbar-btn">
+                    <span class="glyphicon glyphicon-save"></span>
+                </button>
+            </div>
+            <div id="panel-tools" class="btn-group" role="group">
+                <button id="btn-pencil" type="button" class="btn btn-default navbar-btn">
+                    <span class="glyphicon glyphicon-pencil"></span>
+                </button>
+                <button id="btn-erase" type="button" class="btn btn-default navbar-btn">
+                    <span class="glyphicon glyphicon-erase"></span>
+                </button>
+                <button id="btn-font" type="button" class="btn btn-default navbar-btn">
+                    <span class="glyphicon glyphicon-font"></span>
+                </button>
+
+            </div>
+
+            <div id="panel-tools-2" class="btn-group" role="group">
+                <button id="btn-hide" type="button" class="btn btn-default navbar-btn">
+                    <span class="glyphicon glyphicon-eye-close"></span>
+                </button>
+            </div>
+
         </div>
     </div>
 </nav>
@@ -52,6 +92,12 @@ $tileSize = 16;
         </div>
     </div>
 </div>
+<div id="popover-content">
+    <div class="form-group">
+        <textarea class="form-control" rows="2" cols="10"></textarea>
+    </div>
+</div>
+
 <script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.slim.min.js"></script>
 
 <!-- Latest compiled and minified JavaScript -->
--- public/js/main.js
+++ public/js/main.js
@@ -2,19 +2,63 @@
  * Created by aguidet on 27/10/16.
  */
 
-var isClicked = false
+// editMode available are
+// pencil, erase, annotate
+var editMode = ''
+var isCLicked = false
 
+// listen buttons
+$('#panel-tools button').click(function (e) {
+    $('#panel-tools button').removeClass('active')
+    $(this).addClass('active')
+    editMode = $(this).attr('id')
+})
+
+// action
 $('td').hover(function (e) {
-    if (isClicked) {
-        $(this).addClass('grey')
+    if (isCLicked) {
+        switch (editMode) {
+            case 'btn-pencil':
+                $(this).addClass('grey')
+                break
+            case 'btn-erase':
+                $(this).removeClass('grey')
+                break
+        }
     }
 })
 
+$('td').click(function (e) {
+    if (editMode === 'btn-font') {
+        $(this).popover({
+            container: 'body',
+            //html: true,
+            content: 'je suis un commentaire sur cette cellule',
+            placement: 'top'
+        })
+        $(this).popover('show')
+    }
+})
+
+$('#btn-hide').click(function (e) {
+
+    if ($(this).hasClass('active')) {
+
+        $('td').popover('hide')
+        $(this).removeClass('active')
+    } else {
+
+        $('td').popover('show')
+        $(this).addClass('active')
+    }
+
+    e.stopPropagation()
+})
+
 $('body').mousedown(function (e) {
-    isClicked = true
+    isCLicked = true
 })
 
 $('body').mouseup(function (e) {
-    isClicked = false
+    isCLicked = false
 })
-
